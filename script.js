399a6ecd436a4474f42e926f0e13999f35e1318d
stephb0125 <s3735950@students.fscj.edu>
2026-04-26 19:33:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Portfolio Loaded");
+
+// Simple smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
